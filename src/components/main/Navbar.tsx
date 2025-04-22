6debba362813df68ff8317d537e0b79e2aac0c2d
유지민 <a01040987587@gmail.com>
2025-04-22 16:03:00 +0900

    refactor: link로 감싸기

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -1,20 +1,26 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export function Navbar() {
     return (
-        <header className="w-full h-12 border-b bg-blue-600 text-white text-bold px-4 pu-2 flex items-center justify-between">
+        <header className="w-full h-12 border-b bg-blue-600 text-white text-bold px-4 py-2 flex items-center justify-between">
             <div className="flex-xl font-bold">비트포럼</div>
             <nav className="flex justify-center flex-1 gap-4">
-                <Button variant="ghost"
-                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">홈</Button>
-                <Button variant="ghost"
-                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">뉴스</Button>
-                <Button variant="ghost"
-                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">코인정보</Button>
-                <Button variant="ghost"
-                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">스케쥴</Button>
-                <Button variant="ghost"
-                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">로그인</Button>
+                <Link href="/" passHref>
+                    <Button variant="ghost" className="text-white text-bold hover:bg-transparent hover:text-gray-200">홈</Button>
+                </Link>
+                <Link href="/news" passHref>
+                    <Button variant="ghost" className="text-white text-bold hover:bg-transparent hover:text-gray-200">뉴스</Button>
+                </Link>
+                <Link href="/coin-info" passHref>
+                    <Button variant="ghost" className="text-white text-bold hover:bg-transparent hover:text-gray-200">코인정보</Button>
+                </Link>
+                <Link href="/schedule" passHref>
+                    <Button variant="ghost" className="text-white text-bold hover:bg-transparent hover:text-gray-200">스케쥴</Button>
+                </Link>
+                <Link href="/login" passHref>
+                    <Button variant="ghost" className="text-white text-bold hover:bg-transparent hover:text-gray-200">로그인</Button>
+                </Link>
             </nav>
         </header>
     )
