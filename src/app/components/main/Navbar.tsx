19a5e7eca44b43363c7d7bd51fae4a09d56c3bf7
유지민 <a01040987587@gmail.com>
2025-04-22 15:28:42 +0900

    chore: 자잘한 수정

--- src/app/components/main/Navbar.tsx
+++ src/app/components/main/Navbar.tsx
@@ -2,19 +2,19 @@ import { Button } from "@/components/ui/button";
 
 export function Navbar() {
     return (
-        <header className="w-full border-b bg-blue-600 text-white text-bold px-4 pu-2 flex items-center justify-between">
+        <header className="w-full h-12 border-b bg-blue-600 text-white text-bold px-4 pu-2 flex items-center justify-between">
             <div className="flex-xl font-bold">비트포럼</div>
             <nav className="flex justify-center flex-1 gap-4">
                 <Button variant="ghost"
-                    className="text-white hover:bg-transparent hover:text-gray-200">홈</Button>
+                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">홈</Button>
                 <Button variant="ghost"
-                    className="text-white hover:bg-transparent hover:text-gray-200">뉴스</Button>
+                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">뉴스</Button>
                 <Button variant="ghost"
-                    className="text-white hover:bg-transparent hover:text-gray-200">코인정보</Button>
+                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">코인정보</Button>
                 <Button variant="ghost"
-                    className="text-white hover:bg-transparent hover:text-gray-200">스케쥴</Button>
+                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">스케쥴</Button>
                 <Button variant="ghost"
-                    className="text-white hover:bg-transparent hover:text-gray-200">로그인</Button>
+                    className="text-white text-bold hover:bg-transparent hover:text-gray-200">로그인</Button>
             </nav>
         </header>
     )
--- src/app/components/main/banner/RightPanel.tsx
+++ src/app/components/main/banner/RightPanel.tsx
@@ -6,7 +6,7 @@ import { NoticeBanner } from "./NoticeBanner";
 
 export function RightPanel() {
   return (
-    <aside className="w-[340px] ml-6 mt-12 space-y-4 sticky top-4 h-fit">
+    <aside className="w-[360px] ml-6 mt-12 space-y-4 sticky top-4 h-fit">
       <NoticeBanner />
       <CoinInfoList />
     </aside>
