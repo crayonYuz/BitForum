7ccfa03022ebb9051dc837fea48950930dfa6ea3
유지민 <a01040987587@gmail.com>
2025-05-02 15:48:56 +0900

    chore: 동일한 이름/이메일로 로그인시 안내 메시지

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,11 +5,13 @@ import { Button } from '@/components/ui/button';
 import { useSession, signIn } from 'next-auth/react';
 import { MessageCircleMore, Speech, Chrome } from 'lucide-react';
 import { Navbar } from '@/components/main/Navbar';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { GoogleLoginButton } from '@/components/auth/GoogleLoginButton';
 
-export default function LoginPage() {
+export default function Page() {
     const { data: session } = useSession();
+    const searchParams = useSearchParams();
+    const error = searchParams.get('error');
     const router = useRouter();
 
     useEffect(() => {
@@ -27,6 +29,11 @@ export default function LoginPage() {
             <Navbar />
             <main className="flex justify-center items-center min-h-screen pt-14 bg-white px-4">
                 <div className="w-full max-w-md space-y-6 text-center">
+                    {error === 'OAuthAccountNotLinked' && (
+                        <div className="bg-red-100 border border-red-300 text-red-700 p-3 rounded-md text-sm whitespace-pre-line">
+                            이미 동일한 이메일로 가입된 계정이 있습니다. <br /> 기존 계정으로 로그인한 후 소셜 계정을 연결하세요.
+                        </div>
+                    )}
                     <div>
                         <h1 className="text-2xl font-bold">코인 선물 거래의 시작 <br /> 비트포럼에 오신 것을 환영합니다</h1>
                         <p className="text-gray-500 text-sm mt-1">비트포럼은 코인거래 초보자들을 위한 <br />다양한 정보와 뉴스를 제공해드립니다</p>
