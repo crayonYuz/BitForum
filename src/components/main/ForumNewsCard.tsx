7d12ebc18cb879bc154aaa085daa88dbd5730a61
유지민 <a01040987587@gmail.com>
2025-05-02 13:39:50 +0900

    chore: 비트포럼 뉴스 카드 수정

--- src/components/main/ForumNewsCard.tsx
+++ src/components/main/ForumNewsCard.tsx
@@ -1,75 +1,75 @@
 'use client'
 
-import * as React from "react"
-import Image from 'next/image';
-import { Card, CardContent } from "@/components/ui/card"
+import * as React from "react";
+import Image from "next/image";
 import {
     Carousel,
     CarouselContent,
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 
 export function ForumNewsCards() {
     const articles = [
         {
-            title: '쉽고 빠르게 현재 "비트 코인" 현황을 체크해보자',
-            image: 'https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp',
+            author: "실전파이터_카뎃머",
+            title: "5월 2일 비트코인: 신규 진입이 아닌 '익절'을 고려할 시기입니다",
+            image: "https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp",
         },
         {
-            title: '트럼프, TRUMP 보유자 만찬 안 올 수도',
-            image: 'https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp',
+            author: "월스트리트맨",
+            title: "OpenAI CEO 프로젝트 'World', 드디어 미국 진출",
+            image: "https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp",
         },
         {
-            title: '$TRUMP, 대통령과의 식사권',
-            image: 'https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp',
+            author: "나스닥전략가",
+            title: "2025.05.01 나스닥 이슈 및 지수 분석",
+            image: "https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp",
         },
         {
-            title: '트럼프, TRUMP 보유자 만찬 안 올 수도',
-            image: 'https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp',
+            author: "나스닥전략가",
+            title: "2025.05.01 나스닥 이슈 및 지수 분석",
+            image: "https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp",
         },
         {
-            title: '$TRUMP, 대통령과의 식사권',
-            image: 'https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp',
-        },
-        {
-            title: '$TRUMP, 대통령과의 식사권',
-            image: 'https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp',
+            author: "나스닥전략가",
+            title: "2025.05.01 나스닥 이슈 및 지수 분석",
+            image: "https://storage.cobak.co/webp_thumbnails/1745293260575467_22164af347_thumb.webp",
         },
     ];
 
     return (
         <Carousel
-            opts={{
-                align: "start",
-            }}
+            opts={{ align: "start" }}
             className="w-full max-w-5xl mx-auto"
         >
             <CarouselContent>
                 {articles.map((article, index) => (
                     <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                        <div className="p-4">
-                            <Card className="w-80 h-62 border-">
-                                <CardContent className="flex flex-col items-center justify-center p-4">
+                        <div className="p-2">
+                            <div className="w-full overflow-hidden rounded-md shadow-sm">
+                                <div className="relative w-full h-40 cursor-pointer">
                                     <Image
                                         src={article.image}
                                         alt={article.title}
-                                        width={300}
-                                        height={150}
-                                        className="object-cover w-full h-40 mb-4"
+                                        fill
+                                        className="object-cover"
                                     />
-                                    <div className="text-center text-sm font-medium overflow-hidden whitespace-nowrap text-ellipsis">
+                                </div>
+                                <div className="p-3 space-y-1">
+                                    <p className="text-xs text-gray-500">{article.author}</p>
+                                    <p className="text-sm font-semibold line-clamp-1">
                                         {article.title}
-                                    </div>
-                                </CardContent>
-                            </Card>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </CarouselItem>
                 ))}
             </CarouselContent>
-            <CarouselPrevious className="ml-8 cursor-pointer" />
-            <CarouselNext className="mr-8 cursor-pointer" />
+            <CarouselPrevious className="ml-10" />
+            <CarouselNext className="mr-10" />
         </Carousel>
     );
 }
