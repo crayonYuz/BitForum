9a288d164e1093e96230e0fb66548c24d8c32748
유지민 <a01040987587@gmail.com>
2025-05-28 15:58:41 +0900

    fix: 빌드 오류 수정

--- src/app/coin-info/page.tsx
+++ src/app/coin-info/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { getCryptoPrices } from "@/lib/fetchCoins";
 import { Coin, SortType } from "@/types/coin";
