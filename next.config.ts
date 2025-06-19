6d9f8e6367b4a4ab3a052f07aa82bb7b884282a3
유지민 <a01040987587@gmail.com>
2025-06-19 16:32:43 +0900

    feat: 자잘한 수정

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['storage.cobak.co'],
+    domains: ['storage.cobak.co', 'bitforum.blog'],
   },
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Page() {
 
 
             <section>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10 mt-14">
                 <UsStockNewsSection />
                 <CoinNewsSection />
               </div>
